3f8ba270b94e556f9d35c4ba3ea1e54df24c81bc
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2021-12-02 15:15:31 -0600

    style changes and comments added

--- src/mips-registers.ts
+++ src/mips-registers.ts
@@ -1,5 +1,6 @@
-export class MIPS_Registers{
+export class MIPS_Registers {
     // I am not sure if it works this way, but it looks like declaring these here does not affect the abstract properties
+    // Abstract properties are not allowed in Java. Are they allowed in TS?
     toIndex: Map< string, number >;
     data: Array< Array< number > >;
 
@@ -7,28 +8,43 @@ export class MIPS_Registers{
         //super();
         // Initializing values in registers with zeros
         
-        this.data = new Array< Array< number > >(32); // not sure if this will work
+        this.data = new Array< Array< number > >( 32 );
+        /*
+        For the previous line and the commented block of code that follows:
+        JS has a rather clumsy way to handle uninitialized objects. In the previous
+        line I had to indicate the number of them. Otherwise I would have had to create
+        them one by one, in a loop similar to the one below. I think there is a better way
+        to do this. Anyway, writting this reminded me of the old days of Java. I have to say
+        I do not like to program like this anymore and perhaps slightly regret switching to
+        TypeScript. In something more Pythonic, I could just go
+        for i in range(32):
+            t = new Array< number >();
+            t.push( 0 );
+            data.push( t );
+
+        I guess there is no way to avoid hardcoding the 32 - I mean, after all there are 32 registers.
+        I think the code is good as is, but definetely a point of reflection.
+        Thinking further about it, this is the only way. From a memory/object point of view, no language
+        or technology can improve this, because this is as good as it gets. To have an array of k objects
+        you need to create/assign the k objects and then assign them to the array. That is the only way.
+        It still pisses me off that TS failed silently when I was trying to push on an undefined obj.
+        */
+
         /*
         this.data.forEach( register_value_array => {
             register_value_array = new Array< number >();
             register_value_array.push( 0 );
         });*/
 
-        for(let i=0; i < 32; i++) {
-            this.data[i] = new Array< number >();
-            this.data[i].push( 0 );
+        for( let i = 0; i < 32; i++ ) {
+            this.data[ i ] = new Array< number >();
+            this.data[ i ].push( 0 );
         }
 
         let register_names = ['zero','at','v0','v1','a0','a1','a2','a3','t0','t1','t2','t3','t4','t5','t6','t7','s0','s1','s2','s3','s4','s5','s6','s7','t8','t9','k0','k1','gp','sp','fp','ra'];
         this.toIndex = new Map< string, number >();
         register_names.forEach( ( k, v ) => {
             this.toIndex.set( k, v );
-        });
-    }
-
-    show() {
-        this.toIndex.forEach( (v, k) => {
-            console.log(k,' ',v);
         });
     }
 }
