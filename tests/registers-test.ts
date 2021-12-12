99332084834d9040a7aeee341a2bb335c672e239
Uriel Gonzalez <gonzalez.nunez.uriel@gmail.com>
2021-12-12 15:37:28 -0600

    Implemented a test for the proper init of register names
    I find it redundant, since I copy pasted the register names from the constructor. I guess this is what they mean when they say that 100% code coverage is unnecesary.

--- tests/registers-test.ts
+++ tests/registers-test.ts
@@ -69,6 +69,11 @@ function proper_init() {
     it('There are 32 register names', function() {
         expect( registers.toIndex.size ).to.equal( 32 );
     });
-    // This one is going to be tedious, so I left it for latter
-    //it('The register names are mapped correctly');
+    
+    it('The register names are mapped correctly', function() {
+        let register_names = ['$zero','$at','$v0','$v1','$a0','$a1','$a2','$a3','$t0','$t1','$t2','$t3','$t4','$t5','$t6','$t7','$s0','$s1','$s2','$s3','$s4','$s5','$s6','$s7','$t8','$t9','$k0','$k1','$gp','$sp','$fp','$ra'];
+         registers.toIndex.forEach((v,k) => {
+             expect( register_names.includes( k ) ).to.equal( true );
+         });
+    });
 }
