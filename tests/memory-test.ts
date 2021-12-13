7db4075cfa36feccb6d6210bf3ba05726b3037c2
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2021-12-13 11:11:43 -0600

    Changed memory tests to be more robust

--- tests/memory-test.ts
+++ tests/memory-test.ts
@@ -11,6 +11,7 @@ describe('Memory Tests', function() {
 
     beforeEach( function() {
         memory = new Memory();
+        memory.load_source_into_memory( source );
     });
 
     it('Loading source into memory', function() {
@@ -18,7 +19,6 @@ describe('Memory Tests', function() {
         let expected_data = [ 10, 12 ];
         let expected_text = [ 'lw $t0,0($zero)', 'lw $t1,4($zero)', 'add $t2,$t1,$t0', 'sw $t2,0($sp)', 'halt' ];
     
-        memory.load_source_into_memory( source );
         expect( memory.data ).to.eql( expected_data );
         expect( memory.text ).to.eql( expected_text );
     });
@@ -30,8 +30,7 @@ describe('Memory Tests', function() {
         previous one fails. Otherwise, we would think that two things are broken when it could be the case that it is
         only one.
         */
-        memory.load_source_into_memory( source );
-
+       
         let [ data_root, text_root, stack_root ] = memory.getRootAddresses();
         expect( data_root ).to.equal( 0 );
         expect( text_root ).to.equal( 8 );
@@ -40,23 +39,21 @@ describe('Memory Tests', function() {
     });
 
     it('Get instructions at root', function() {
-        memory.load_source_into_memory( source );
-
+        
         let some_instruction_at_root = memory.get( 8 );
         
         expect( some_instruction_at_root ).to.eql( 'lw $t0,0($zero)' );
     });
 
     it('Get instructions', function() {
-        memory.load_source_into_memory( source );
-
+        
         let some_instruction = memory.get( 20 ); 
         
         expect( some_instruction ).to.eql( 'sw $t2,0($sp)' );
     });
 
     it('Get values at root', function() {
-        memory.load_source_into_memory( source );
+        
 
         let some_data_at_root = memory.get( 0 );
 
@@ -64,7 +61,6 @@ describe('Memory Tests', function() {
     });
 
     it('Get values', function() {
-        memory.load_source_into_memory( source );
         
         let some_data = memory.get( 4 );
 
@@ -72,7 +68,7 @@ describe('Memory Tests', function() {
     });
 
     it('Set value', function() {
-        memory.load_source_into_memory( source );
+        
         let value  = 32;
         let address = 4;
         
